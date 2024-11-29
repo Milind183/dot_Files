926cef79fc9d2a868f7c9e3117f15426a088048e
Milind Rathod <100861819+Milind183@users.noreply.github.com>
2024-11-30 00:47:01 +0530

    Update imp_notes.sh

--- imp_notes.sh
+++ imp_notes.sh
@@ -2,9 +2,9 @@
 
 #packages - 
 	   :dwm - patches 				#fullgaps
-	   :oh-my-posh  				#catppuccin_macchito 
+	   :oh-my-posh  				#catppuccin_macchito check the themes properly
 	   :oh-my-zsh 					#catppuccin_macchito #autosuggestions #zshhighligiting
 	   :picom by implement-window-animations 	#I have uploaded the .config for this
 	   :nvim -					#I have uploaded the .config for this
-	   :kitty 					#nothing much here just the theme	
+	   :kitty 					#nothing much here just the theme
 

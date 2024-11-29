f4daf32a9791684b3f778cb8b45a5383a4960d79
Milind183 <milindrathod183@gmail.com>
2024-11-30 01:04:17 +0530

    minor changes

--- imp_notes.sh
+++ imp_notes.sh
@@ -4,7 +4,8 @@
 	   :dwm - patches 				#fullgaps
 	   :oh-my-posh  				#catppuccin_macchito check the themes properly
 	   :oh-my-zsh 					#catppuccin_macchito #autosuggestions #zshhighligiting
-	   :picom by implement-window-animations 	#I have uploaded the .config for this
-	   :nvim -					#I have uploaded the .config for this
+	   						#https://github.com/JanDeDobbeleer/oh-my-posh.git 
+	   :picom by implement-window-animations 	#https://github.com/EdenQwQ/picom.git
+	   :nvim 					#I have uploaded the .config for this
 	   :kitty 					#nothing much here just the theme
-
+	   :Main font 					#JETBARINS

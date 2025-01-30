7de5e6d20992aacab23a526143191f53ac07197f
Milind Rathod <100861819+Milind183@users.noreply.github.com>
2025-01-30 18:40:34 +0530

    Update mybuild.sh

--- mybuild.sh
+++ mybuild.sh
@@ -47,7 +47,7 @@ install_dependencies() {
 		libvterm unibilium tree-sitter cmake ninja pkgconf picom libconfig libev \
 		libgl libepoxy pcre2 pixman xcb-util-image xcb-util-renderutil dbus \
 		xorg-xprop xorg-xwininfo rtkit meson git xorgproto libxext asciidoc \
-		neovim kitty nodejs npm
+		neovim kitty nodejs npm dmenu xclip 
 
 	yay -S --noconfirm uthash
 	echo "Dependencies installed successfully."
